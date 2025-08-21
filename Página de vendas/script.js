3f160aca24b99d2559923c22ca41e2dfeb3dcc48
Arthur Leandro <arthurfonseca263@gmail.com>
2025-08-21 11:26:42 -0300

    script.js

--- Página de vendas/script.js
+++ Página de vendas/script.js
@@ -2,19 +2,8 @@ const btnComprar = document.getElementById ('btn-comprar');
 btnComprar.addEventListener('click', function() {
     const nome = document.getElementById('nome').value;
     const email = document.getElementById('email').value;
-    const telefone = document.getElementById('telefone').value;
-
-    if (nome && email && telefone) {
-        alert(`Obrigado pela compra, ${nome}!`);
-    } else {
-        alert('Por favor, preencha todos os campos.');
-    }
-});const btnComprar = document.getElementById ('btn-comprar');
-btnComprar.addEventListener('click', function() {
-    const nome = document.getElementById('nome').value;
-    const email = document.getElementById('email').value;
-    const telefone = document.getElementById('telefone').value;
-
+    const telefone = document.getElementById('telefone').value; 
+    
     if (nome && email && telefone) {
         alert(`Obrigado pela compra, ${nome}!`);
     } else {
